cfc7f05ddc6f3e64dd7db33d9fc6529b00adb78c
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-17 20:41:01 -0700

    can add and delete from list, but can make it more dynamic and functional

--- app.js
+++ app.js
@@ -12,13 +12,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// const items = ["Buy Food", "Cook Food", "Eat Food"];
-// const workItems = [];
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false},);
 
 const Schema = mongoose.Schema;
-
 
 const itemsSchema = {
   name: {
@@ -40,9 +37,6 @@ const item3 = new Item({
 })
 
 const defaultItems= [item1, item2, item3]
-
-
-
 
 
 app.get("/", function(req, res) {
