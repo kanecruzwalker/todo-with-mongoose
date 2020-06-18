2ce0388eaa17cd18a5c5960e995ad51cf980e1ab
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-17 22:59:25 -0700

    added modular port to accomodate heroku

--- app.js
+++ app.js
@@ -139,7 +139,12 @@ app.get("/:newList", function (req,res){
 
 });
 
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+app.listen(port, function() {
+  console.log("Server started successfully on" + port);
 });
