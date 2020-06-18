43353f62348a4760c1d8da1c15ce12852b01bb48
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-17 21:49:32 -0700

    required lodash

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 // const date = require(__dirname + "/date.js");
 const mongoose= require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
